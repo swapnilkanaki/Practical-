ff13361d115d72b20bef3ad17da24d12b28dd584
swapnilkanaki <53909864+swapnilkanaki@users.noreply.github.com>
2023-03-15 13:56:48 +0530

    Arithmetic_opre.java added

---
+++ Arithmetic_opre.java
@@ -0,0 +1,11 @@
+public class Arithmetic_opre {
+	public static void main(String[] args) {
+	System.out.println(" Values a = 28 and  b = 46");
+	int a=28;
+	int b=46;
+	System.out.println(" a + b = "+(a+b));
+	System.out.println(" a - b = "+(a-b));
+	System.out.println(" a * b = "+(a*b));
+	System.out.println(" a / b = "+(a/b));
+	}
+}
